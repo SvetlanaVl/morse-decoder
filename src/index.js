5b0c4823b281c726edd5eb21bf2ac5664a99d11f
Svetlana <fidelity-67rus@mail.ru>
2022-08-04 14:07:19 +0300

    feat: add test

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = expr.match(/.{1,10}/g);
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === '**********') {
+            arr[i] = ' ';
+        }
+        arr[i] = arr[i].replace(/10/g, '.');
+        arr[i] = arr[i].replace(/11/g, '-');
+        arr[i] = arr[i].replace(/00/g, '');
+    
+        for (var key in MORSE_TABLE) {
+            if(arr[i] === key) {
+                arr[i] = MORSE_TABLE[key];
+            }
+        }
+    }
+    return arr.join('');
 }
 
 module.exports = {
